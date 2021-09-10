803c67c26e4aad8b0ba5398cac28ee24a23b6701
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-09-09 21:03:30 -0300

    Feitos exs 1001/02/03/04

---
+++ URI_ ONLINE/1_iniciante_001.py
@@ -0,0 +1,23 @@
+# 1001
+A = int(input("a"))
+B = int(input("b"))
+X = A + B
+print(f'X = {X}')
+
+# 1002
+r = float(input("Digite o raio: "))
+π = 3.14159
+a = π * (r**2)
+print(f"A={a:.4f}")
+
+# 1003
+A = int(input())
+B = int(input())
+SOMA = A + B
+print(f'SOMA = {SOMA}')
+
+# 1004
+A = int(input())
+B = int(input())
+PROD = A * B
+print(f'PROD = {PROD}')
