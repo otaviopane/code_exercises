90a22a331dba09599ce099443e0de4dd9473c688
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-09-11 02:44:59 -0300

    Feito exs ate o 1015, criado arquivo de teste e organizado exs em arquivos de 10 em 10

---
+++ URI_ ONLINE/1_iniciante_000.py
@@ -0,0 +1,59 @@
+# 1001
+A = int(input("a"))
+B = int(input("b"))
+X = A + B
+print(f'X = {X}')
+
+# 1002
+r = float(input("Digite o raio: "))
+π = 3.14159
+a = π * (r**2)
+print(f"A={a:.4f}")
+
+# 1003
+A = int(input())
+B = int(input())
+SOMA = A + B
+print(f'SOMA = {SOMA}')
+
+# 1004
+A = int(input())
+B = int(input())
+PROD = A * B
+print(f'PROD = {PROD}')
+
+# 1005
+A = float(input())
+B = float(input())
+MEDIA = ((A*3.5) + (B*7.5))/11
+print(f'MEDIA = {MEDIA:.5f}')
+
+# 1006
+A = float(input())
+B = float(input())
+C = float(input())
+MEDIA = ((A*2)+(B*3)+(C*5))/10
+print(f'MEDIA = {MEDIA:.1f}')
+
+# 1007
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+DIFERENCA = (A * B - C * D)
+print(f'DIFERENCA = {DIFERENCA}')
+
+# 1008
+NUMBER = int(input())
+hor_trab = int(input())
+val_hor = float(input())
+SALARY = hor_trab * val_hor
+print(f'NUMBER = {NUMBER}')
+print(f'SALARY = U$ {SALARY:.2f}')
+
+# 1009
+nome = str(input())
+sal = float(input())
+vend = float(input())
+tot = sal + (vend/100*15)
+print(f'TOTAL = R$ {tot:.2f}')
--- URI_ ONLINE/1_iniciante_001.py
+++ URI_ ONLINE/1_iniciante_001.py
@@ -1,23 +1,50 @@
-# 1001
-A = int(input("a"))
-B = int(input("b"))
-X = A + B
-print(f'X = {X}')
+# 1010
+pcas1 = input().split()
+pcas2 = input().split()
+valor = (float(pcas1[1])*float(pcas1[2]))+(float(pcas2[1])*float(pcas2[2]))
+print(f'VALOR A PAGAR: R$ {valor:.2f}')
 
-# 1002
-r = float(input("Digite o raio: "))
-π = 3.14159
-a = π * (r**2)
-print(f"A={a:.4f}")
+# 1011
+R = float(input())
+pi = 3.14159
+vol = (4/3) * pi * R**3
+print(f'VOLUME = {vol:.3f}')
 
-# 1003
-A = int(input())
-B = int(input())
-SOMA = A + B
-print(f'SOMA = {SOMA}')
+# 1012
+val = input().split()
+A = float(val[0])
+B = float(val[1])
+C = float(val[2])
+pi = 3.14159
+tri = (A*C)/2
+cir = pi * C**2
+tra = (A+B)/2*C
+qua = B**2
+ret = A*B
 
-# 1004
-A = int(input())
-B = int(input())
-PROD = A * B
-print(f'PROD = {PROD}')
+print(f'TRIANGULO: {tri:.3f}')
+print(f'CIRCULO: {cir:.3f}')
+print(f'TRAPEZIO: {tra:.3f}')
+print(f'QUADRADO: {qua:.3f}')
+print(f'RETANGULO: {ret:.3f}')
+
+# 1013
+a, b, c = input().split()
+a, b, c = int(a), int(b), int(c)
+val = sorted([a, b, c])
+print(f'{val[-1]} eh o maior')
+
+# 1014
+X = int(input())
+Y = float(input())
+cons = X / Y
+print(f'{cons:.3f} km/l')
+
+# 1015
+p1 = input().split()
+p2 = input().split()
+x1, y1, x2, y2 = float(p1[0]), float(p1[1]), float(p2[0]), float(p2[1])
+dist = ((x2-x1)**2 + (y2-y1)**2)**0.5
+print(f'{dist:.4f}')
+
+# 1016
